1c383548ce00397725b0f605208b8553c7763ff5
Vinnichowit23 <vinnichowdaryit23@gmail.com>
2023-07-14 12:44:45 -0500

    Rolutte code added

---
+++ Training/new/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/Training/First"
+	"example.com/Training/First/Second"
+	"example.com/Training/Functions"
+	"example.com/Training/Packages"
+	"example.com/Training/Rolutte"
+	"example.com/Training/Welcome"
+	"example.com/Training/arrays"
+	"example.com/Training/loops"
+)
+
+func main() {
+	fmt.Println("")
+	Welcome.Wel1()
+	Welcome.T1()
+	Packages.P1()
+	Packages.P2()
+	Packages.P3()
+	Packages.P4()
+	Functions.F1()
+	Functions.F2()
+	Functions.F3()
+	Functions.F4()
+	Functions.F5()
+	Functions.F6()
+	arrays.A1()
+	First.F1()
+	Second.S1()
+	Rolutte.R1()
+	loops.L1()
+}
